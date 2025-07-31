4e7784ac4b27f7b65c8c91193b9c9d9698a8096c
Michael Terry <michael.terry@childrens.harvard.edu>
2025-07-31 10:45:12 -0400

    Bump version

--- fhirclient/client.py
+++ fhirclient/client.py
@@ -1,7 +1,7 @@
 import logging
 from .server import FHIRServer, FHIRUnauthorizedException, FHIRNotFoundException
 
-__version__ = '4.3.1'
+__version__ = '4.3.2'  # Update docs/Doxyfile too when you bump this
 __author__ = 'SMART Platforms Team'
 __license__ = 'APACHE2'
 __copyright__ = "Copyright 2017 Boston Children's Hospital"
